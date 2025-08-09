5e8156340009f629a4aac4a7dc6c615361c8b84c
mahatosourav00 <mahatosourav01@gmail.com>
2025-08-09 22:50:59 +0530

    update c

--- effect.js
+++ effect.js
@@ -120,7 +120,7 @@ $('document').ready(function(){
 
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
-		$('#b1, #b4, #b5, #b7, #b9').addClass('balloons-rotate-behaviour-one');
+		$('#b1, #b4, #b5, #b7').addClass('balloons-rotate-behaviour-one');
 		$('#b2, #b3, #b6, #b8').addClass('balloons-rotate-behaviour-two');
 		// $('#b3').addClass('balloons-rotate-behaviour-two');
 		// $('#b4').addClass('balloons-rotate-behaviour-one');
@@ -135,7 +135,7 @@ $('document').ready(function(){
 		loopSix();
 		loopSeven();
 		loopEight();
-		loopNine();
+		// loopNine();
 		
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#cake_fadein').fadeIn('slow');
@@ -160,7 +160,7 @@ $('document').ready(function(){
 	$('#wish_message').click(function(){
 		 vw = $(window).width()/2;
 
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8,#b9').stop();
+		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7,#b8').stop();
 		$('#b1').attr('id','b11');
 		$('#b2').attr('id','b22')
 		$('#b3').attr('id','b33')
@@ -169,7 +169,7 @@ $('document').ready(function(){
 		$('#b6').attr('id','b66')
 		$('#b7').attr('id','b77')
 		$('#b8').attr('id','b88')
-		$('#b9').attr('id','b99')
+		// $('#b9').attr('id','b99')
 		$('#b11').animate({top:240, left: vw-250},500);
 		$('#b22').animate({top:240, left: vw-200},500);
 		$('#b33').animate({top:240, left: vw-150},500);
@@ -178,7 +178,7 @@ $('document').ready(function(){
 		$('#b66').animate({top:240, left: vw+0},500);
 		$('#b77').animate({top:240, left: vw+50},500);
 		$('#b88').animate({top:240, left: vw+100},500);
-		$('#b99').animate({top:240, left: vw+150},500);
+		// $('#b99').animate({top:240, left: vw+150},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
